6bcb5746a967ea02cba88648f8232b8f45959404
anthony fargnoli <fargnoli.anthony@gmail.com>
2020-03-10 14:44:37 +1100

    added track endpoint and file log

--- tracker.site/src/index.js
+++ tracker.site/src/index.js
@@ -1,26 +1,49 @@
-const express = require( "express" );
+const express = require("express");
+const moment = require("moment");
 const app = express();
 const port = 8080; // default port to listen
+const fs = require('fs');
+
+startupTask = () => {
+
+    if (!fs.existsSync("log.txt")) {
+
+        fs.open('log.txt', 'w', function (err, file) {
+            if (err) throw err;
+            //console.log('Saved!');
+        });
+    }
+
+}
+
+startupTask();
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
 
 const trackImg = new Buffer("R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7", "base64");
-app.get( "/track", ( req, res ) => {
+app.get("/track", (req, res) => {
     res.writeHead(200, {
         "Content-Length": trackImg.length,
         "Content-Type": "image/gif"
     });
 
-const address = req.connection.remoteAddress;
-const ip = req.ip;
+    const address = req.connection.remoteAddress;
+    const ip = req.ip;
 
-res.end(trackImg);
-} );
+    fs.appendFile("log.txt", `${address} ${ip} ${moment(Date.now()).format()}\n`, (err) => {
+        
+            //onsole.log(err);
+    });
+    
+    res.end(trackImg);
+
+
+});
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
